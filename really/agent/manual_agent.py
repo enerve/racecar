317ad6a6ae0f53af34c16ffd165553ae694d5887
enerve <10472438+enerve@users.noreply.github.com>
2019-09-14 16:38:02 +0800

    Update ManualAgent

--- really/agent/manual_agent.py
+++ really/agent/manual_agent.py
@@ -5,73 +5,41 @@ Created on Sep 6, 2018
 '''
 
 import logging
-import numpy as np
-import random
-from driver import Driver
+from .agent import Agent
 
-from environment import Environment
-import util
-
-class ManualDriver(Driver):
+class ManualAgent(Agent):
     '''
-    An agent that drives with the given fixed sequence of actions.
+    An agent that follows the given fixed sequence of actions.
     '''
 
     def __init__(self,
-                 num_junctures,
-                 num_lanes,
-                 num_speeds,
-                 num_directions,
-                 num_steer_positions,
-                 num_accel_positions,
+                 config,
                  action_sequence):
         '''
         Constructor
         '''
-        super().__init__(num_junctures,
-                         num_lanes,
-                         num_speeds,
-                         num_directions,
-                         num_steer_positions,
-                         num_accel_positions)
+        super().__init__()
         
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
-    
-        util.pre_alg = "test_driver"
+
+        self.action_sequence = action_sequence        
+
+    def init_episode(self, initial_state):#, initial_heights):
+        ''' Initialize for a new episode
+        '''
+        super().init_episode(initial_state)
+
+        self.actions_taken = 0
+        self.A = None
+
+    def see_outcome(self, reward, new_state, moves=0):
+        super().see_outcome(reward, new_state, moves)
+        self.logger.debug(" A:%s  R:%d    S:%s" % (self.A, self.G, new_state))
+
+    def _choose_action(self):
+        # Pick the next action in the given sequence
+        self.A = self.action_sequence[self.actions_taken]
+        self.actions_taken += 1
         
-        self.action_sequence = action_sequence
-        
-    def run_episode(self, track, car):
-        environment = Environment(track,
-                                  car,
-                                  self.num_junctures,
-                                  should_record=True)
-
-        total_R = 0
-        for A in self.action_sequence:
-            R, S_ = environment.step(A)
-            self.logger.debug(" A:%s  R:%d    S:%s" % (A, R, S_,))
-            if S_ is None:
-                break
-            total_R += R
-
-        return total_R, environment
-    
-    def collect_stats(self, ep, num_episodes):
-        pass
-    
-    def save_stats(self, pref=None):
-        pass
-
-    def load_stats(self, subdir, pref=None):
-        pass
-
-    def report_stats(self, pref):
-        pass
-
-    def save_model(self, pref=""):
-        pass
-
-    def load_model(self, load_subdir):
-        pass
+        return self.A
